b8111c69fad2d13c8edbc38d701382d0ff0613ed
ClarisNjoga <mimonjoga@gmail.com>
2023-03-08 00:44:23 +0300

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,87 @@
+require "date"
+
 puts "🌱 Seeding spices..."
 
-# Seed your database here
-50.times do
-    User.create({username: Faker::Name.first_name, password: Faker::Name.last_name})
-end
 
-Task.create({name: "Shopping", status:"Complete", description: "easy", duedate:12032021, user_id:User.all.sample.id,})
-Task.create({name: "Read", status:"Complete", description: "intermediate", duedate:15032021, user_id:User.all.sample.id, })
-Task.create({name: "Hiking", status:"Incomplete", description: "difficult", duedate:17032021,  user_id:User.all.sample.id})
-Task.create({name: "Yoga", status:"Complete", description: "intermediate", duedate:20032021, user_id:User.all.sample.id})
-Task.create({name: "Watch movie", status:"Complete", description: "easy", duedate:21032021, user_id:User.all.sample.id})
+User.create(name: "John")
+User.create(name: "Mike")
+User.create(name: "Dan")
+
+Task.create(
+    {name: "Mow Lawn",
+    description: "Mow front and back lawns, trim hedges",
+    category_id: 1,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "Make Dinner",
+    description: "Thaw chicken, chop veggies, make stew",
+    category_id: 1,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "Get groceries",
+    description: "Buy produce, snacks, and supplies",
+    category_id: 3,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "Pick up drycleaning",
+    description: "Cleaners on 4th Ave",
+    category_id: 3,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "Feed dogs",
+    description: nil,
+    category_id: 1,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "TPS Reports",
+    description: "Finish TPS reports, send to Bill",
+    category_id: 2,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "Staff Meeting",
+    description: nil,
+    category_id: 2,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+
+Category.create(
+  name: "Home"
+)
+
+Category.create(
+  name: "Work"
+)
+
+Category.create(
+  name: "Personal"
+)
+
 
 puts "✅ Done seeding!"
